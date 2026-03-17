d3bef7488522b8b8d96974e2e6bfa03d3e476d9d
Matt <matt4real@yahoo.com>
2026-03-17 23:10:22 +0100

    feat: add logic for win condition

--- box-battle/src/App.jsx
+++ box-battle/src/App.jsx
@@ -3,6 +3,14 @@ import GameBoard from './components/GameBoard.jsx';
 import Player from './components/Player.jsx';
 import Log from './components/Log.jsx';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
@@ -15,16 +23,47 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard; // copy
+
+  if (gameTurns.length > 0) {
+    for (const turnObj of gameTurns) {
+      const { square, playerIdentity } = turnObj;
+      const { row, col } = square;
+
+      gameBoard[row][col] = playerIdentity;
+    }
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handlePlayerSwitch(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
 
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
-      
+
       const updatedTurns = [
         {
           square: { row: rowIndex, col: colIndex },
@@ -54,7 +93,9 @@ function App() {
           />
         </ol>
 
-        <GameBoard onPlayerSwitch={handlePlayerSwitch} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+
+        <GameBoard onPlayerSwitch={handlePlayerSwitch} gameBoard={gameBoard} />
       </div>
 
       <Log turns={gameTurns} />
--- box-battle/src/components/GameBoard.jsx
+++ box-battle/src/components/GameBoard.jsx
@@ -1,36 +1,6 @@
 import { useState } from 'react';
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-function GameBoard({ onPlayerSwitch, turns }) {
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleCellClick(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [...prevGameBoard.map((innerArr) => [...innerArr])];
-
-  //     updatedBoard[rowIndex][colIndex] = activePlayer;
-  //     return updatedBoard;
-  //   });
-
-  //   onPlayerSwitch();
-  // }
-
-  let gameBoard = initialGameBoard; // copy
-
-  if (turns.length > 0) {
-    for (const turnObj of turns) {
-      const { square, playerIdentity } = turnObj;
-      const { row, col } = square;
-
-      gameBoard[row][col] = playerIdentity;
-    }
-  }
-
+function GameBoard({ onPlayerSwitch, gameBoard }) {
   return (
     <ol id="game-board">
       {gameBoard.map((rowArr, rowIndex) => (
@@ -38,7 +8,10 @@ function GameBoard({ onPlayerSwitch, turns }) {
           <ol>
             {rowArr.map((cellVal, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onPlayerSwitch(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onPlayerSwitch(rowIndex, colIndex)}
+                  disabled={cellVal !== null}
+                >
                   {cellVal}
                 </button>
               </li>
